7b097412ddce3814b41740c667c6f62250c90e33
“Krista <stadler.krista@gmail.com>
2021-02-16 21:19:18 -0700

    set up models

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_many :messages, class_name: "Message", foreign_key: "recipient_id"
+  has_many :sent_messages, class_name: "Message", foreign_key: "sender_id"
+end
