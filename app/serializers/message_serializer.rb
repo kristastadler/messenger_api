19b531ebf2b532651db3171d956fff7c53addf5f
“Krista <stadler.krista@gmail.com>
2021-02-16 22:16:24 -0700

    refactored to include more information in json response

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer
   include FastJsonapi::ObjectSerializer
   set_type :message
-  attributes :content, :sender_id, :recipient_id
-end 
+  attributes :content, :sender, :recipient, :created_at, :updated_at
+end
