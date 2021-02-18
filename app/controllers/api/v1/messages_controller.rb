de766899b93695ba4e77bd57308288bbdc60782f
“Krista <stadler.krista@gmail.com>
2021-02-17 20:28:27 -0700

    added sad path testing for unknown sender and recipient

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,14 @@ class Api::V1::MessagesController < ApplicationController
 
   def create
     message = Message.create(message_params)
-    render json: MessageSerializer.new(message)
+    if message.sender != nil && message.recipient != nil
+      render json: MessageSerializer.new(message)
+    else
+      create_error = {
+        "error_message" => "Oops, looks like at least one of those users doesn't exist!"
+      }
+      render json: create_error, status: 404
+    end
   end
 
   private
--- spec/api/v1/post_message_spec.rb
+++ spec/api/v1/post_message_spec.rb
@@ -9,10 +9,36 @@ describe "Messages API" do
                                        sender_id: sam.id,
                                        recipient_id: frodo.id }
     message = Message.last
-    parse_message = JSON.parse(response.body)
+    body = JSON.parse(response.body)
     expect(response).to be_successful
     expect(message.content).to eq("Boil 'em, mash 'em, stick 'em in a stew!")
     expect(message.recipient.username).to eq("shireguy")
     expect(message.sender.username).to eq("potatolover")
   end
-end 
+
+  it "displays an error if the sender doesn't exist" do
+    frodo = User.create!(username: "shireguy")
+
+    post "/api/v1/messages", params: { content: "We've got to get the Ring back to Mordor, ASAP!",
+                                       sender_id: frodo.id,
+                                       recipient_id: 7 }
+
+    body = JSON.parse(response.body)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+    expect(body["error_message"]).to eq("Oops, looks like at least one of those users doesn't exist!")
+  end
+
+  it "displays an error if the recipient doesn't exist" do
+    frodo = User.create!(username: "shireguy")
+
+    post "/api/v1/messages", params: { content: "We've got to get the Ring back to Mordor, ASAP!",
+                                       sender_id: 18,
+                                       recipient_id: frodo.id }
+
+    body = JSON.parse(response.body)
+    expect(response).to_not be_successful
+    expect(response.status).to eq(404)
+    expect(body["error_message"]).to eq("Oops, looks like at least one of those users doesn't exist!")
+  end
+end
